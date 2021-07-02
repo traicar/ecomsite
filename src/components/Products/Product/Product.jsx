698780b45866a8095c26a65dd69528100e688616
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2021-07-02 06:35:34 -0500

    images

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -7,17 +7,17 @@ const Product = ({ product }) => {
     const classes= useStyles();
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardMedia className={classes.media} image={product.image} title={product.name} />
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
                         {product.name}
                     </Typography>
                     <Typography variant="h5">
-                        {product.name}
+                        {product.price}
                     </Typography>
                 </div>
-                <Typography variant="h2" color='textSecondary'>
+                <Typography variant="body2" color='textSecondary'>
                     {product.description}
                 </Typography>
             </CardContent>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
+import useStyles from './styles'
+
 
 const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes', price: '$5' },
-    { id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10' }
+    { id: 1, name: 'Cupcakes', description: 'Bite sized cupcakes', price: '$3', image:'../../assets/cupcakes.JPG' },
+    { id: 2, name: 'Cakes', description: '7 inch cakes', price: '$40', image:'../../assets/creamcakes.JPG' },
 ]
 
 const Products = () => {
+    const classes=useStyles();
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
             <Grid container justify='center' spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
