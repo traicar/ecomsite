ea4e527a9236844ab6e46e477e9ceb06fe309c23
Tanavi Raicar <81321683+traicar@users.noreply.github.com>
2021-09-14 16:15:47 -0500

    ecom complete, payment not working

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppBar, Toolbar, IconButton, MenuItem, Badge, Menu, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons'
 import logo from '../../assets/logo.png'
@@ -6,8 +6,28 @@ import useStyles from './styles'
 import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = ({ totalItems }) => {
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
     const classes = useStyles();
     const location = useLocation();
+
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+    const handleMobileMenuClose = () => setMobileMoreAnchorEl(null);
+
+    const mobileMenuId = 'primary-search-account-menu-mobile';
+
+    const renderMobileMenu = (
+        <Menu anchorEl={mobileMoreAnchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} id={mobileMenuId} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={isMobileMenuOpen} onClose={handleMobileMenuClose}>
+            <MenuItem>
+                <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                    <Badge badgeContent={totalItems} color="secondary">
+                        <ShoppingCart />
+                    </Badge>
+                </IconButton>
+                <p>Cart</p>
+            </MenuItem>
+        </Menu>
+    );
 
     return (
         <>
@@ -26,7 +46,7 @@ const Navbar = ({ totalItems }) => {
                                 </Badge>
                             </IconButton>
                         </div>)}
-                    </Toolbar>
+                </Toolbar>
             </AppBar>
         </>
     )
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,15 +3,16 @@ import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
 import useStyles from './styles'
 
-const Products = ({products, onAddToCart}) => {
-    const classes=useStyles();
+const Products = ({ products, onAddToCart }) => {
+    const classes = useStyles();
+    if (!products.length) return <p>Loading...</p>;
     return (
         <main className={classes.content}>
-            <div className={classes.toolbar}/>
+            <div className={classes.toolbar} />
             <Grid container justify='center' spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} onAddToCart={onAddToCart}/>
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
